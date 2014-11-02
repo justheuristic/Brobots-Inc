aed983d8bad921e06b72db79d2cc72cd909a5b16
sekam94 <sekam94@gmail.com>
2014-11-02 22:31:25 +0300

    up
    up

---
+++ Game/game/IO/Screen.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+using game.Logic;
+using game.Objects;
+
+
+namespace game.IO
+{
+    public class ScreenManager: Manager
+    {
+
+        public ScreenManager(MainGame game):base(game){}
+        SpriteBatch batch;
+		
+        SortedDictionary<int, HashSet<GameObject>> objects = new SortedDictionary<int, HashSet<GameObject>>();
+        public void RegisterObject(GameObject obj)
+        {
+            if (!objects.ContainsKey(obj.drawPriority))
+                objects[obj.drawPriority] = new HashSet<GameObject>();
+            objects[obj.drawPriority].Add(obj);
+        }
+        public void UnregisterObject(GameObject obj)
+        {
+            objects[obj.drawPriority].Remove(obj);
+            //not trying to remove empty sets as this is saving us time for their recreation for the cost of a few memory.
+        }
+
+        public void LoadTextures(ContentManager Content, SpriteBatch _spritebatch, GraphicsDevice device)
+        {
+            batch = _spritebatch;
+            foreach (var kvp in objects)
+            {
+                foreach (GameObject obj in kvp.Value)
+                {
+                    obj.InitTexture(Content,device);
+                    
+                }
+            }
+
+
+        }
+        public override void Loop(float dtime)
+        {
+            HashSet<GameObject> destroyed = new HashSet<GameObject>();
+            foreach (var kvp in objects)
+            {
+                foreach (GameObject obj in kvp.Value)
+                {
+                    obj.Draw(batch);
+                    if (obj.destroyed)
+                        destroyed.Add(obj);
+                        
+                }
+            }
+            foreach( GameObject obj in destroyed)
+            {
+                UnregisterObject(obj);
+            }
+
+        }
+    }
+}
---
+++ Game/game/Packages/BackgroundTest.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+using game.Objects;
+using game.Loaders;
+
+namespace game.Packages.Background
+{
+    class backgroundBuilder
+    {
+        public static void addBackground(MainGame game, string tex = "BG")
+        {
+            backgroundObject obj = new backgroundObject(game.ScreenWidth,game.ScreenHeight,tex);
+            game.addGameObject(obj);
+
+            
+        }
+    }
+    class backgroundObject: GameObject
+    {
+        
+
+        public backgroundObject(int targWidth = int.MinValue, int targHeight = int.MinValue, string texName = "BG")
+            : base()
+        {
+            targetHeight = targHeight;
+            targetWidth = targWidth;
+            drawPriority = int.MinValue;
+            textureName = texName;
+        }
+        string textureName;
+        int targetWidth, targetHeight;
+        public override void InitTexture(ContentManager Content, GraphicsDevice device)
+        {
+            Texture = Content.Load<Texture2D>(textureName);
+            base.InitTexture(Content,device);
+            if (targetWidth != int.MinValue)
+                size.Width = targetWidth;
+            if (targetHeight != int.MinValue)
+                size.Height = targetHeight;
+            Position = new Vector2(size.Width/2, size.Height/2);
+            
+            
+        }
+    }
+}
